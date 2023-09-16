0fee6adfaece3060cd372f7906de6867d64497c2
abelqasm <abelqasm@c4r6p4.1337.ma>
2023-09-16 13:15:10 +0100

    adding Dark Mode

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/rawg-logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+
+const NavBar = () => {
+	return (
+		<HStack justifyContent="space-between">
+			<Image src={logo} />
+			<ColorModeSwitch />
+		</HStack>
+	);
+};
+
+export default NavBar;
