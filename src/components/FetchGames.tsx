6311f1d56d3099bfd630cb65fd37b57511761ea3
abelqasm <abelqasm@c4r5p4.1337.ma>
2023-09-17 12:05:16 +0100

    add a box container to CardGame and CardGame skeleton

--- src/components/FetchGames.tsx
+++ src/components/FetchGames.tsx
@@ -2,19 +2,27 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const FetchGames = () => {
 	const { games, error, isLoading } = useGames();
-	const skeletons = [1, 2, 3, 4, 5, 6];
+	const skeletons = [1, 2, 3, 4, 5];
 
 	console.log(isLoading);
 	return (
 		<>
 			{error && <Text>{error}</Text>}
 			<SimpleGrid columns={{ sm: 1, md: 3, lg: 5 }} padding={10} spacing={10}>
-				{isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
+				{isLoading &&
+					skeletons.map((skeleton) => (
+						<GameCardContainer>
+							<GameCardSkeleton key={skeleton} />
+						</GameCardContainer>
+					))}
 				{games.map((game) => (
-					<GameCard key={game.id} game={game}></GameCard>
+					<GameCardContainer>
+						<GameCard key={game.id} game={game}></GameCard>
+					</GameCardContainer>
 				))}
 			</SimpleGrid>
 		</>
