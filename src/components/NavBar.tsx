c7fca24c0428b42e1ce8649eb9d6abc4ecbca8fa
Abderrahmane Belqasmi <abelqasm@e2r6p3.1337.ma>
2023-10-11 22:45:12 +0100

    fixing nav bar and logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,15 @@
 import { HStack, Image } from '@chakra-ui/react';
-import logo from '../assets/rawg-logo.webp';
+import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchBar from './SearchBar';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 	return (
 		<HStack padding="10px">
-			<Image src={logo} boxSize="40px" borderRadius={8} />
+			<Link to="/">
+				<Image src={logo} boxSize="40px" borderRadius={8} objectFit="cover" />
+			</Link>
 			<SearchBar />
 			<ColorModeSwitch />
 		</HStack>
