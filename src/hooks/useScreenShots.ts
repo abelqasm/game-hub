ecb2324b64e264b6d53bb7d201813bdaecc67a1f
Abderrahmane Belqasmi <abelqasm@e2r6p3.1337.ma>
2023-10-11 22:29:37 +0100

    add screenshots of game

---
+++ src/hooks/useScreenShots.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import { ScreenShot } from '../entities/ScreenShot';
+import APIClient from '../services/api-client';
+
+const useScreenShots = (gameId: number) => {
+	const apiClinet = new APIClient<ScreenShot>(`/games/${gameId}/screenshots`);
+
+	return useQuery({
+		queryKey: ['screnshots', gameId],
+		queryFn: apiClinet.getAll,
+	});
+};
+
+export default useScreenShots;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,18 +4,20 @@ import useGame from '../hooks/useGame';
 import { Heading, Spinner } from '@chakra-ui/react';
 import GameAtributes from '../components/GameAttributes';
 import GameTrailer from '../components/GameTrailer';
+import GameScreenShots from '../components/GameScreenShots';
 
 const GameDetailPage = () => {
 	const { slug } = useParams();
 	const { data: game, isLoading, error } = useGame(slug!);
 
 	if (isLoading) return <Spinner />;
-	if (error || !game) throw error; 
+	if (error || !game) throw error;
 	return (
 		<>
 			<Heading>{game.name}</Heading>
 			<ExpandableText content={game.description_raw} />
 			<GameAtributes game={game} />
+			<GameScreenShots gameId={game.id} />
 			<GameTrailer gameId={game.id} />
 		</>
 	);
