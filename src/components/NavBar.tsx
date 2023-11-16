556abc5b8db806a1f274a02977ae9d2cdfe9acdc
abelqasm <abderrahmane.belqasmi@gmail.com>
2023-11-16 14:25:04 +0100

    change logo to red

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,4 @@
 import { HStack, Image } from '@chakra-ui/react';
-import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchBar from './SearchBar';
 import { Link } from 'react-router-dom';
@@ -8,7 +7,7 @@ const NavBar = () => {
 	return (
 		<HStack padding="10px">
 			<Link to="/">
-				<Image src={logo} boxSize="40px" borderRadius={8} objectFit="cover" />
+				<Image src='/game-hub-logo.svg' boxSize="40px" borderRadius={8} objectFit="cover" />
 			</Link>
 			<SearchBar />
 			<ColorModeSwitch />
